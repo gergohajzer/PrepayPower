b518f81ede7b9d53110a2826c42c71f2f2771598
Gergo Hajzer <gergohajzer@gmail.com>
2023-03-24 16:28:50 +0100

    init using clean architecture

---
+++ tests/Application.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PrepayPower.Application.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
