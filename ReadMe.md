96aaba46623c634e5e1e34f07f9b2c1a031af201
Gergo Hajzer <gergohajzer@gmail.com>
2023-03-26 17:18:03 +0200

    Presentation layer.

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+﻿# PrepayPower technical assessment
+
+This solution is made by Gergo Hajzer in 26/03/2023.
+
+To run the application you need to run the following command:
+
+`dotnet run --project ./src/ConsoleApp/ "${path_to_vegetables_import_csv_file}" "${path_to_purchase_import_csv_file}" "${output_folder}"`
+
+${path_to_vegetables_import_csv_file} is the file path to your csv file that contains vegetables and their prices in the following format:
+
+````
+PRODUCT,PRICE
+Tomato,0.75
+Aubergine,0.9
+Carrot,1
+````
+
+${path_to_purchase_import_csv_file} is the file path to your csv file that represents a purchase and contains vegetables and their quantity in the following format:
+
+````
+PRODUCT,QUANTITY
+Tomato,3
+Aubergine,25
+Carrot,12
+````
+${output_folder} is the path for your output folder.
--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,3 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PrepayPower.Application.Offers;
+using PrepayPower.Application.Purchases.Commands;
+using PrepayPower.Application.Purchases.Queries;
+using PrepayPower.Application.Vegetables.Commands;
+using PrepayPower.Domain.Entities;
 
-Console.WriteLine("Hello, World!");
+if (args.Length < 2)
+{
+    Console.Error.WriteLine("Please provide a file containing vegetables, a file containing a purchase and a directory where you want to export your receipt.");
+    return -1;
+}
+if (!File.Exists(args[0]))
+{
+    Console.Error.WriteLine($"File does not exist at path: '{args[0]}'.");
+    return -1;
+}
+if (!File.Exists(args[1]))
+{
+    Console.Error.WriteLine($"File does not exist at path: '{args[1]}'.");
+    return -1;
+}
+if (!Directory.Exists(args[2]))
+{
+    Console.Error.WriteLine($"Directory does not exist at path: '{args[2]}'.");
+    return -1;
+}
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationServices();
+        services.AddInfrastructureServices();
+    })
+    .Build();
+
+using IServiceScope serviceScope = host.Services.CreateScope();
+var provider = serviceScope.ServiceProvider;
+var mediator = provider.GetRequiredService<IMediator>();
+
+await mediator.Send(new ImportVegetablesCommand(args[0]));
+
+var purchase = await mediator.Send(new ImportPurchaseCommand(args[1]));
+
+var offers = new List<Offer>
+{
+    new ("TO001", "If you buy at least 5 tomato, we will give you one for free.", TomatoOffers.OneFreeAfterFive),
+    new ("PO001", "Every third potato is free.", PotatoOffers.EveryThirdIsFree),
+    new ("CO001", "For every 5€ spent on Tomatoes, we will deduct one euro.", CarrotOffers.AfterEvery5EurDeduct1Eur)
+};
+
+await mediator.Send(new ApplyOffersOnPurchaseCommand(offers, purchase));
+
+var receipt = await mediator.Send(new GetReceiptForPurchaseCommand(purchase));
+
+await using var writer = new StreamWriter(Path.Combine(args[2], $"{purchase.Date:yyyy-MM-dd-HH-mm-ss}_{Guid.NewGuid()}.txt"));
+writer.Write(receipt);
+
+return 0;
